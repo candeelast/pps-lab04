0e17b8665fcf36c6753da2ffec29c8631b0d896a
candeelast <131805947+candeelast@users.noreply.github.com>
2025-04-03 16:22:17 +0200

    task5

--- src/main/scala/tasks/typeclasses/Ex5Traversable.scala
+++ src/main/scala/tasks/typeclasses/Ex5Traversable.scala
@@ -1,6 +1,9 @@
 package u04lab
-import u03.Sequences.* 
+import u03.Sequences.*
 import Sequence.*
+import u03.Optionals.*
+import u03.Optionals.Optional.Just
+import u04lab.Ex5Traversable.Traversable
 
 /*  Exercise 5: 
  *  - Generalise by ad-hoc polymorphism logAll, such that:
@@ -17,10 +20,36 @@ import Sequence.*
 
 object Ex5Traversable:
 
+  trait Traversable[T[_]]:
+    def logAll[A](t: T[A]): Unit
+
   def log[A](a: A): Unit = println("The next element is: "+a)
 
-  def logAll[A](seq: Sequence[A]): Unit = seq match
-    case Cons(h, t) => log(h); logAll(t)
-    case _ => ()
 
-  
+
+  given Traversable[Sequence] with
+    def logAll[A](t: Sequence[A]): Unit = t match
+      case Cons(h, t) => log(h); logAll(t)
+      case _ => ()
+
+
+  given Traversable[Optional] with
+    def logAll[A](t: Optional[A]): Unit = t match
+      case Optional.Just(a) => println(a)
+      case _ => ()
+
+  def generalLog[A, T[_] : Traversable](cont: T[A]): Unit =
+    val traversableInstance = summon[Traversable[T]]
+    traversableInstance.logAll(cont)
+
+@main def tryTraversable =
+  val seq = Cons(10, Cons(20, Cons(30, Nil())))
+  val traversableSeq = summon[Traversable[Sequence]]
+  traversableSeq.logAll(seq) // Logs: "The next element is: 10", etc.
+
+  val opt = Just(42)
+  val traversableOpt = summon[Traversable[Optional]]
+  traversableOpt.logAll(opt) // Logs: "42"
+
+  val emptyOpt = Optional.Empty()
+  traversableOpt.logAll(emptyOpt) // Logs nothing
